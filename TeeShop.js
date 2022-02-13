3a2f70c93f1081c0d2a50e5a66691a3910979346
dev0ps221 <seybaxscorpion@gmail.com>
2022-02-13 11:20:33 +0000

    database - ready :: ear

--- TeeShop.js
+++ TeeShop.js
@@ -1,13 +1,39 @@
 const DeeBee = require("@tek-tech/deebee")
-class TeeShop{
+const {Ear} = require("@tek-tech/ears")
+
+class TeeShop extends Ear{
 
 
-    
-    constructor(dbcreds={}){
+    initDeeBee(dbcreds={}){
         this.database = new DeeBee(dbcreds);
-        
+        this.whenDatabaseReady(
+            ()=>{
+                this.dbready = 1 
+                console.log("database ready")
+            }
+        )
+    }
+    whenDatabaseReady(action){
+        this.database.whenReady(
+            action
+        )
+    }
+    init(dbcreds){
+        this.initDeeBee(dbcreds)
+    
+        this.whenReady(
+            ()=>{
+                console.log("i am ready")
+            }
+        )
+    }
+    constructor(dbcreds){
+        super()
+        this.init(dbcreds)
     }
 
 }
 
 module.exports = TeeShop
+
+let tshop = new TeeShop({host:'127.0.0.1',user:'root',database:'tek_tech',password:''})
